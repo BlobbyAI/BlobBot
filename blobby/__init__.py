ad648efe7ad3a767a7172d2cd548f92a5b0833e1
devops117 <55235206+devops117@users.noreply.github.com>
2022-11-19 12:07:26 +0530

    set uvloop as optional dependency
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- blobby/__init__.py
+++ blobby/__init__.py
@@ -1,7 +1,9 @@
-import asyncio
+import contextlib
 
-from tornado.platform.asyncio import AsyncIOMainLoop
-import uvloop
+with contextlib.suppress(ImportError):
+    import uvloop
+    import asyncio
+    from tornado.platform.asyncio import AsyncIOMainLoop
 
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-AsyncIOMainLoop().install()
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    AsyncIOMainLoop().install()
