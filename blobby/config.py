29f7e993b649bdfdc85f60697f18eb56f3934784
devops117 <55235206+devops117@users.noreply.github.com>
2022-11-19 17:51:56 +0530

    remove name and prompt from the AIProfile and configs
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- blobby/config.py
+++ blobby/config.py
@@ -7,24 +7,20 @@ from typing import BinaryIO
 class AIProfile:
     """
     Prompts:
-    name: name of the bot, it may change how the bot percieves itself.
-    prompt: describes how the bot should respond, it works best with a name. E.g 'marv is a really rude bot'.
-    conversation_buffer_size: Buffer for the amount of conversations to be kept in memory.
+    conv_buf_size: Buffer for the amount of conversations to be kept in memory.
     model: name of the model (see https://beta.openai.com/docs/models/gpt-3).
 
     Warning:
     keep the prompt short, as it counts towards every single API request.
-    keep the context size small, it may increase the chargers.
+    keep the conv_buf_size small, as it may increase the charges.
 
-    Example presets:
-    marv_preset = {name: 'marv', model: 'text-davinci-002', context: 1, prompt: 'marv is quite rude'}
-    blob_preset = {name: 'blob', model: 'text-davinci-002', context: 2, prompt: 'blob is really friendly and jokes a lot'}
+    Example profiles:
+    marv_preset = {model: 'text-davinci-002', conv_buf_size: 1}
+    blob_preset = {model: 'text-davinci-002', conv_buf_size: 2}
     """
 
-    name: str
-    prompt: str
     model: str
-    conversation_buffer_size: int
+    conv_buf_size: int
 
 
 def from_toml(tomlfile: BinaryIO) -> AIProfile:
